7d7bf94c761050afe730e87b58d757eac83d84eb
JuyoungYang <juyoung.yang11@gmail.com>
2025-02-21 10:29:43 +0900

    fix: deleted dest.py and my_project folder (erroneously created and pushed by junho)

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # recipe_app
+    path("api/", include("recipe_app.urls")),
+    # chatbot_app
+    path("api/", include("chatbot_app.urls")),
 ]
--- dest.py
+++
@@ -1,6 +0,0 @@
-import chardet
-
-with open("data/recipe_01.csv", "rb") as f:
-    result = chardet.detect(f.read())
-
-print(f"Detected encoding: {result['encoding']}")
