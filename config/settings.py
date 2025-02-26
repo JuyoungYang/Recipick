8b4a1ac6ffc09b5ecba882eae84f91d0e14159db
JuyoungYang <juyoung.yang11@gmail.com>
2025-02-26 10:57:11 +0900

    new django secret_key

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j*o^*!oo7a3s%)2v&q_so0e$l9i%xv(ka5i-x-!9e^i+9bh7&t"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
